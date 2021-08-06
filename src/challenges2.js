0ffa897c2c4acad8380bb4f224cf2fcdef2bbf97
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-08-06 14:52:33 -0300

    Raphael Taglialegna -Bonus  Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,11 +6,11 @@ function techList(techProp, name) {
   // Organiza do objeto.
   techProp.sort();
   let arrayObject = [];
-  // Foi usado o forEach para percorrer o array.  
+  // Foi usado o forEach para percorrer o array.
   techProp.forEach((content) => {
-    let totalContent = { // Cria um objeto. 
+    let totalContent = { // Cria um objeto.
       tech: content,
-      name: name,
+      name,
     };
     arrayObject.push(totalContent);
   });
@@ -18,8 +18,44 @@ function techList(techProp, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function generatePhoneNumber(arrayNumbers) {
+  let phoneNumber = '';
+  if (arrayNumbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (arrayNumbers[index] < 0 || arrayNumbers[index] > 9 || repeatNumber(arrayNumbers) === true) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    if (index === 0) {
+      phoneNumber = `(${arrayNumbers[index]}${arrayNumbers[index + 1]}) `;
+    } if (index >= 2 && index <= 6) {
+      phoneNumber += arrayNumbers[index];
+    } if (index === 7) {
+      phoneNumber += `-${arrayNumbers[index]}`;
+    } if (index >= 8) {
+      phoneNumber += arrayNumbers[index];
+    }    
+  }
+  return phoneNumber;
+}
+
+function repeatNumber(arrayNumbers) {
+  let countRepeat = {};
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (countRepeat[arrayNumbers[index]]) {
+      countRepeat[arrayNumbers[index]] += 1;
+    } else {
+      countRepeat[arrayNumbers[index]] = 1;
+    }
+  }
+  for (let indexCount in countRepeat) {
+    if (countRepeat[indexCount] >= 3) {
+      return true;
+    }
+  }
+  return false;
 }
 
 // Desafio 12
